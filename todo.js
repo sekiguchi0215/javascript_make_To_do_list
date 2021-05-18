bd9b3243a9d0540f90658fa8f46fe69ce404458e
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-19 01:44:36 +0900

    ToDoリスト その１ 途中

---
+++ todo.js
@@ -0,0 +1,28 @@
+const addButton = document.querySelector('.todo-button')
+const todoInput = document.querySelector('.todo-input')
+const todoList = document.querySelector('.todo-list')
+
+const addTodo = () => {
+  // alert(`タスク追加:${todoInput.value}`)
+  const newTodo = document.createElement('li')
+
+  const todoContent = document.createElement('span')
+  todoContent.innerText = todoInput.value
+  todoContent.classList.add('todo-content')
+  newTodo.appendChild(todoContent)
+
+  todoList.appendChild(newTodo)
+  const checkButton = document.createElement('button')
+  checkButton.innerHTML = '□未着手'
+  checkButton.classList.add('check-button')
+  newTodo.appendChild(checkButton)
+
+  const deleteButton = document.createElement('button')
+  deleteButton.innerHTML = '削除'
+  deleteButton.classList.add('delete-button')
+  newTodo.appendChild(deleteButton)
+
+  todoList.appendChild(newTodo)
+}
+
+addButton.addEventListener('click', addTodo)
